0be980f3ffc5bf54e69143a6c80ca2a00fe628f8
TreeKR <redwikitv@gmail.com>
2020-09-25 10:03:29 +0900

    redas

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.get('/unverify', (req,res) => {
 })
 
 app.get('/support', (req, res) => {
-    return res.redirect(200, 'https://skhcs.com/skyisle')
+    return res.redirect('https://skhcs.com/skyisle')
 })
 
 app.post('/verify', (req,res) => {
